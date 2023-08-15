bc025c26e8157505c700a9b9c60195b5eb504879
Barra Santi <barrasanti@gmail.com>
2023-08-15 10:29:55 -0300

    Added Window class

---
+++ Engine/src/Window.cpp
@@ -0,0 +1,27 @@
+﻿#include "Window.h"
+
+Window::Window(int width, int height, const char* title)
+{
+    window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+}
+
+Window::~Window()
+{
+    glfwDestroyWindow(window);
+    window = nullptr;
+}
+
+bool Window::shouldClose() const
+{
+    return glfwWindowShouldClose(window);
+}
+
+GLFWwindow* Window::getWindow() const
+{
+    return window;
+}
+
+void Window::swapBuffers() const
+{
+    glfwSwapBuffers(window);
+}
---
+++ Engine/src/Window.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <glfw3.h>
+
+class Window
+{
+public:
+    Window(int width, int height, const char* title);
+    ~Window();
+    
+    bool shouldClose() const;
+    void swapBuffers() const;
+    
+    GLFWwindow* getWindow() const;
+private:
+    
+    GLFWwindow* window;
+};
