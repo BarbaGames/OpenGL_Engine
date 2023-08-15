b7325e544051280fe14176ba7592db7b5aa00ed5
Mau <mauvepinktree@gmail.com>
2023-08-15 10:55:13 -0300

    Added comments

--- Engine/src/Test.cpp
+++ Engine/src/Test.cpp
@@ -3,20 +3,24 @@
 
 int main(void)
 {
+    // Initiating glfw context
     glfwInit();
 
+    // Creating window
     Window window(640, 480, "Hello World");
 
+    // Verifying window
     if (!window.getWindow())
     {
         glfwTerminate();
         return -1;
     }
 
+    // Setting window to glfw context
     window.makeContext();
 
     /* Loop until the user closes the window */
-    while (!window.shouldClose())
+    while (!window.shouldClose()) // Verifies if the window closes
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
--- Engine/src/Window.h
+++ Engine/src/Window.h
@@ -7,10 +7,14 @@ public:
     Window(int width, int height, const char* title);
     ~Window();
     
+    // Returns if the window was closed
     bool shouldClose() const;
+    // Swaps front buffer with back buffer
     void swapBuffers() const;
+    // Adds context to the window (Sets it to glfw by default)
     void makeContext() const;
     
+    // Returns the active instance of glfw window
     GLFWwindow* getWindow() const;
 private:
     
