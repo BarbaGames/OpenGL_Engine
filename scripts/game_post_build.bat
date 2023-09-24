20642a748c455e3e871e2f0607b1721a9b78ffc6
Mau <mauvepinktree@gmail.com>
2023-09-24 19:38:52 -0300

    Modified build scripts

--- scripts/game_post_build.bat
+++ scripts/game_post_build.bat
@@ -3,5 +3,7 @@ set solution_dir=%~1
 set platform=%~2
 set config=%~3
 xcopy /e /v /y "%solution_dir%\bin\Engine\%platform%_%config%\Engine.dll" "%solution_dir%\bin\Game\%platform%_%config%\"
+if exist "%solution_dir%\bin\Engine" (rmdir /s /q "%solution_dir%\bin\Engine")
+if exist "%solution_dir%\obj\Engine" (rmdir /s /q "%solution_dir%\obj\Engine")
 
 REM "$(SolutionDir)scripts\game_post_build.bat" "$(SolutionDir)" "$(Platform)" "$(Configuration)"
