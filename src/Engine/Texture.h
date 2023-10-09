0771998f7d02032537fef8c1ef2ec16912e7d068
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2023-10-09 17:58:50 -0300

    Added Texture and image loader

---
+++ src/Engine/Texture.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <string>
+#include "Engine.h"
+
+namespace MyEngine {
+    class DLLExport Texture {
+        
+    private:
+        unsigned int TextureID;
+        unsigned char* TextureData;
+
+    public:
+        int TextureWidth;
+        int TextureHeight;
+        int TextureBitDepth;
+        int TextureChannels;
+
+        std::string TexturePath;
+        bool Transparent;
+
+        Texture();
+        ~Texture();
+
+        bool LoadTexture(const std::string filePath);
+        void UnloadTexture();
+
+        void StartUsingTexture();
+        void StopUsingTexture();
+    };
+};
